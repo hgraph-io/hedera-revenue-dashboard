f19cb04b951758b6fd6f02d28857e90cbca99d19
Tyler McDonald <tyler@hgraph.io>
2024-08-18 18:06:52 -0600

    add additional queries for deposits by period

--- public/hgraph/queries/Deposits.js
+++ public/hgraph/queries/Deposits.js
@@ -1,41 +1,53 @@
 export default `
-query Deposits {
-  node: ecosystem_metric_aggregate(where: {name: {_like: "node_%_deposits"}}) {
+query Deposits($startDate: timestamp) {
+  node: ecosystem_metric_aggregate(
+    where: {name: {_like: "node_%_deposits"}, period: {_eq: "hour"}, start_date: {_gte: $startDate}}
+  ) {
     aggregate {
       sum {
         total
       }
     }
   }
-  atma_node: ecosystem_metric_aggregate(where: {name: {_like: "atma_node_%_deposits"}}) {
+  atma_node: ecosystem_metric_aggregate(
+    where: {name: {_like: "atma_node_%_deposits"}, period: {_eq: "hour"}, start_date: {_gte: $startDate}}
+  ) {
     aggregate {
       sum {
         total
       }
     }
   }
-  staking: ecosystem_metric_aggregate(where: {name: {_eq: "staking_account_deposits"}}) {
+  staking: ecosystem_metric_aggregate(
+    where: {name: {_eq: "staking_account_deposits"}, period: {_eq: "hour"}, start_date: {_gte: $startDate}}
+  ) {
     aggregate {
       sum {
         total
       }
     }
   }
-  atma_staking: ecosystem_metric_aggregate(where: {name: {_eq: "atma_staking_account_deposits"}}) {
+  atma_staking: ecosystem_metric_aggregate(
+    where: {name: {_eq: "atma_staking_account_deposits"}, period: {_eq: "hour"}, start_date: {_gte: $startDate}}
+  ) {
     aggregate {
       sum {
         total
       }
     }
   }
-  treasury: ecosystem_metric_aggregate(where: {name: {_eq: "treasury_account_deposits"}}) {
+  treasury: ecosystem_metric_aggregate(
+    where: {name: {_eq: "treasury_account_deposits"}, period: {_eq: "hour"}, start_date: {_gte: $startDate}}
+  ) {
     aggregate {
       sum {
         total
       }
     }
   }
-  atma_treasury: ecosystem_metric_aggregate(where: {name: {_eq: "atma_treasury_account_deposits"}}) {
+  atma_treasury: ecosystem_metric_aggregate(
+    where: {name: {_eq: "atma_treasury_account_deposits"}, period: {_eq: "hour"}, start_date: {_gte: $startDate}}
+  ) {
     aggregate {
       sum {
         total
